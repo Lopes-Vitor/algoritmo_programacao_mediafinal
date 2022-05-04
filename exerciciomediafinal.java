d8a796711b0fcf8f048cd7e0894a6c0d272d8198
Lopes-Vitor <vitorlopescav@hotmail.com>
2022-05-04 20:23:01 -0300

    Update exerciciomediafinal.java

--- exerciciomediafinal.java
+++ exerciciomediafinal.java
@@ -2,24 +2,33 @@ package exercicios;
 import java.util.Scanner;
 public class exerciciomediafinal {
 	public static void main(String[] args) {
-	// Declarando o Scanner e as variaveis
+	// Declarando o Scanner
     Scanner leitor = new Scanner(System.in);
+    
+    //Declarando as variaveis
     int ac1, ac2, ag, af;
     double peso1, peso2, pesog, pesof, mediafinal; 
-    //imprimindo perguntas para saber qual a nota do aluno em cada avaliacao
+    
+    //imprimindo perguntas para saber qual a nota do aluno na AC1, AC2, AG E AF
     System.out.println("Qual foi a sua nota da AC1:");
+    //Leitura do valor entrado
     ac1 = leitor.nextInt();
+    
     System.out.println("Qual foi a sua nota da AC2:");
     ac2 = leitor.nextInt();
+    
     System.out.println("Qual foi a sua nota da AG:");
     ag = leitor.nextInt();
+    
     System.out.println("Qual foi a sua nota da AF:");
     af = leitor.nextInt();
+    
    //calculo de cada prova por seus respectivos pesos
     peso1 = (0.15 * ac1);
     peso2 = (0.3 * ac2);
     pesog = (0.1 * ag);
     pesof = (0.45 * af);
+    
    //caculo da media final e imprimindo o resultado da media final do aluno
     mediafinal = peso1 + peso2 + pesog + pesof;
     System.out.println("media final do aluno: " + mediafinal);
